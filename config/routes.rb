78c35f79e9aaf6878d8764a55b3c8de6199a2ccf
Mickey Loreti <mloreti10@gmail.com>
2016-02-25 15:36:39 -0800

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+devise_for :admins
+get '/contact', to: 'static_pages#contact'
+
 resources :posts
+root 'posts#index'
+
+get '/images/:id', to: 'posts#show'
+get '/images/new', to: 'posts#new'
+
+get '*path' => redirect('/')
+
 end
