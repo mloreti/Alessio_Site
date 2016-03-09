c47b4ff1ecda9e93632eb28c535677e7f08343d7
Mickey Loreti <mloreti10@gmail.com>
2016-03-09 15:50:51 -0800

    Optimize home page images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_attached_file :image,
-    styles: { original: "100%", large: "600x600>",
-    medium: "300x300>", thumb: "100x100>" }, 
+    styles: { original: "100%",xlarge: "1000x1000>", large: "600x600>",
+    medium: "300x300>", thumb: "100x100>" },
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
