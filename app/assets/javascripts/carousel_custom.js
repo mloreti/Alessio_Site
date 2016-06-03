4d328a419e2c41f813003befdd190740b0118ef1
Mickey Loreti <mloreti10@gmail.com>
2016-06-03 14:00:35 -0700

    Update carousel

---
+++ app/assets/javascripts/carousel_custom.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+     // + any other carousel related stuff that has to wait for the images to complete loading
+     $('.carousel').carousel({
+         interval: 3500 //changes the speed
+     })
+});
