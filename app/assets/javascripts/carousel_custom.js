8ffe77770461c9c943ea53051dad12af73835053
Mickey Loreti <mloreti10@gmail.com>
2016-06-04 00:23:35 -0700

    Update carousel js and nav css

--- app/assets/javascripts/carousel_custom.js
+++ app/assets/javascripts/carousel_custom.js
@@ -1,6 +1,4 @@
 $(window).load(function() {
      // + any other carousel related stuff that has to wait for the images to complete loading
-     $('.carousel').carousel({
-         interval: 3500 //changes the speed
-     })
+
 });
